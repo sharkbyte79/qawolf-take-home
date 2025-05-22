ebb6a526ccc21655a4df996cb702c42ba13a35a0
Layne <pierce77@bu.edu>
2025-05-22 13:30:56 -0400

    Create locator for article submission ages

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 // const { chromium } = require("playwright");
-import { chromium, Browser, BrowserContext, Page } from "playwright";
+import { chromium, Browser, BrowserContext, Page, Locator } from "playwright";
 
-async function sortHackerNewsArticles() {
+async function sortHackerNewsArticles(): Promise<void> {
   // launch browser
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  const browser: Browser = await chromium.launch({ headless: false}); // headless to spawn browser in the background
+  const context: BrowserContext = await browser.newContext();
+  const page: Page = await context.newPage();
 
   // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
+  const link: string = "https://news.ycombinator.com/newest";
+  await page.goto(link);
 
+  console.log("Went to link: %s", link);
+
+  // create locator for submission times. submission date/time has class 'age'
+  const ageLocator: Locator = page.locator("span.age");
+  console.log("Got %d ages", await ageLocator.count());
 }
 
 (async () => {
