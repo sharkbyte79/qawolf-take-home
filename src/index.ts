4a54246a1fd81a8ec384107b9782685211bd5b2e
Layne <pierce77@bu.edu>
2025-05-23 14:09:37 -0400

    Extract article title and submission timestamp, collect into array for comparison

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,75 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 // const { chromium } = require("playwright");
-import { chromium, Browser, BrowserContext, Page, Locator } from "playwright";
+import { chromium, Browser, BrowserContext, Page, Locator } from 'playwright';
+import pino from 'pino';
 
-async function sortHackerNewsArticles(): Promise<void> {
-  // launch browser
-  const browser: Browser = await chromium.launch({ headless: false}); // headless to spawn browser in the background
-  const context: BrowserContext = await browser.newContext();
-  const page: Page = await context.newPage();
-
-  // go to Hacker News
-  const link: string = "https://news.ycombinator.com/newest";
-  await page.goto(link);
-
-  console.log("Went to link: %s", link);
-
-  // for exactly the first 100, we'll need to check the first few pages
-  const agesLocators: Locator[] = [];
-
-  // create locator for submission times. submission date/time has class 'age'
-  while (agesLocators.length < 100) {
-    const ageLocator: Locator = page.locator(".age"); // prefer locator to something like waitForSelector
-    agesLocators.push(...await ageLocator.all()); 
-
-    console.log(agesLocators.length)
-
-    // navigate to next page
-    await page.locator(".morelink").click();
-    // console.log("Navigated to next page")
-  }
-
-  // keep the first 100 results
-  const firstHundredAgeLocators: Locator[] = agesLocators.slice(0,100);
-  console.log(firstHundredAgeLocators);
-
-  await page.close(); // close browser once we've collected the submission ages
+// Descriptive wrapper for the info we'll scrape from each article listing
+// This only requires getting the ages/submission times for comparison, but we'll include
+// the title as well for logging purposes
+interface ArticleData {
+    title: string; // title of the article
+    timestamp: number; // timestamp for the article submission
 }
 
-(async () => {
-  await sortHackerNewsArticles();
-})();
+export default async function sortHackerNewsArticles(
+    page: Page,
+    numSubmissions: number
+): Promise<boolean> {
+    // Returning a boolean to make this testable
+    // launch browser
+
+    // NOTE: We can let the tester handle launching browsers instead of doing it manually
+    // const browser: Browser = await chromium.launch({ headless: false }); // headless=true to spawn browser in the background
+    // const context: BrowserContext = await browser.newContext();
+    // const page: Page = await context.newPage();
+
+    // go to Hacker News
+    const link: string = 'https://news.ycombinator.com/newest';
+    await page.goto(link);
+
+    console.log('Went to link: %s', link);
+
+    // for exactly the first 100, we'll need to check the first few pages
+    let articles: ArticleData[] = [];
+
+    // create locator for submission times. submission date/time has class 'age'
+    while (articles.length < numSubmissions) {
+        const articlesLoc: Locator = await page.locator('.athing'); // prefer locator to something like waitForSelector
+        const articlesCount: number = await articlesLoc.count();
+
+        for (let i = 0; i < articlesCount; i+=1) {
+            const article: Locator = articlesLoc.nth(i);
+
+            const title: string = (await article.locator('.titleline a').first().innerText());
+            const subDate: string = await article.locator('+ tr').locator('.age').getAttribute('title') ?? '';
+            const timestamp: number = parseInt(subDate.split(" ")[1]); // the submission age contains a UNIX timestamp, more useful for direct comparison
+
+            articles.push({title, timestamp});
+
+            if (articles.length >= 100) {
+                break;
+            }
+
+        }
+
+        // navigate to next page
+        await page.getByRole('link', { name: 'More' }).click();
+        console.log('Navigated to next page');
+
+    }
+
+    // keep the first 100 results
+    // const firstHundredAgeLocators: Locator[] = agesLocators.slice(0,100);
+    // console.log(firstHundredAgeLocators);
+
+    await page.close(); // close browser once we've collected the submission dates
+
+    console.log(articles)
+    // console.log(articles);
+
+    return true;
+}
+
+// (async () => {
+//     await sortHackerNewsArticles(100);
+// })();
